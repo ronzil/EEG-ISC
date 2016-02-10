987b35431e0ca23b035bd6cb3776dcf1c26d09e7
Ronen Zilberman <ronen.zilberman@gmail.com>
2016-02-10 14:03:32 -0500

    refactor timetosample finction + bug fix

---
+++ eeg_timetosample.m
@@ -0,0 +1,5 @@
+function res = eeg_timetosample(EEG, position)
+	start = eeg_starttime(EEG)
+	diff = position - start
+	
+	res = round( seconds(diff) * EEG.srate)
--- eeg_trimstart.m
+++ eeg_trimstart.m
@@ -1,12 +1,12 @@
 % trim EEG data to start at specific datetime.
 function res = eeg_trimstart(EEGdata, dtObj)
 
-	start = eeg_starttime(EEGdata);
-	diff = dtObj - start;
-	assert(diff>0, 'Currently no support for negative adjusment');
-	
-	skip = round( seconds(diff) * 1000 / EEGdata.srate);
+	skip = eeg_timetosample(EEGdata, dtObj);
+	assert(skip > 0, 'Currently no support for negative adjusment');
 	disp(sprintf('eeg_trimstart: trimming %d samples.', skip));
+	
+	% trim begining of data
 	res = eeg_eegrej( EEGdata, [1 skip] );
+	
 	% write bvtime back to EEG	
 	res = eeg_starttime_set(res, dtObj);
