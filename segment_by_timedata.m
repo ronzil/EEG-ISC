3c1de7ba810b0647cf54808fd822ffdfb163b824
Ronen Zilberman <ronen.zilberman@gmail.com>
2016-02-27 20:49:18 -0500

    seperated random fucntion

--- segment_by_timedata.m
+++ segment_by_timedata.m
@@ -42,49 +42,22 @@ function [res, segnificance] = do_segment_by_timedata_with_random(timedata, segm
     rval = totaltime - totaldurlen;
    
     for i=1:randnum
-        randts = timedata.startTS + int64(sort(randperm(rval, length(durs))))';
+        randts = timedata.startTS + sort(randperm(rval, length(durs)))';
         randts = randts + cumsum([0;durs(1:end-1)]);
         
         randseg = [randts , durs];
         
-        val(i)= do_segment_by_timedata(timedata, randseg);
+        vec = vector_from_timedata_by_segments(timedata, randseg);
+        val(i)= mean(vec);
     end
     
     mr = mean(val);
     sr = std(val);
     
-    res = do_segment_by_timedata(timedata, segment_data);
+    vec = vector_from_timedata_by_segments(timedata, segment_data);
+    res = mean(vec);
     
     segnificance = (res-mr)/sr;
 
 end
 
-
-
-function res = do_segment_by_timedata(timedata, segment_data)
-
-    step = timedata.step;
-
-	startoffset = round((segment_data(:,1) - timedata.startTS)/step) + 1;
-    endoffset = startoffset + segment_data(:,2)/step - 1;
-    
-    % note. fix this by getting the last chunk of the EEG data
-    startoffset = max(startoffset, 1);
-    endoffset = min(endoffset, length(timedata.data));
-    
-    
-	assert(min(startoffset)>0);
-	assert(max(endoffset)<=length(timedata.data));
-    
-	
-	% go over all the segments
-	for i=1:length(segment_data)
-%        if (endoffset(i) == startoffset(i)) continue; end% note. remove when added chunks 
-
-%		val(i) = mean(timedata.data(startoffset:durationoffset));
-		val(i) = mean(timedata.data(startoffset(i):endoffset(i)));
-	end
-
-	res = mean(val(~isnan(val)));
-
-end
