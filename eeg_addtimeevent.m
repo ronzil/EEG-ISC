f0167995ee5b49e8b872d71b62d1a309d23d1f3a
Ronen Zilberman <ronen.zilberman@gmail.com>
2016-07-29 15:00:04 +1200

    improved addtimeevent

--- eeg_addtimeevent.m
+++ eeg_addtimeevent.m
@@ -6,8 +6,21 @@
 %   title          - name of event
 function EEG = eeg_addtimeevent(EEG, position, duration, title)
 
-	sample = eeg_timetosample(EEG, position)
-	
-	assert(sample>0);
+	% assert input is of the same length
+	assert(length(position) == length(duration));
+	assert(length(position) == length(title));
 
-	EEG = eeg_addnewevents(EEG, {[sample]}, {title}, {'duration'}, {[duration]});
+	% convert timestamps to sample number
+	sample = eeg_timetosample(EEG, position);
+	
+	% remove events that are in the past
+	good_samples = sample>0;
+	sample = sample(good_samples);
+	position = position(good_samples);
+	duration = duration(good_samples);
+	title = title(good_samples);
+	
+	% add events
+	EEG = eeg_addnewevents(EEG,num2cell(sample), title, {'duration'}, {[duration]});
+
+end
