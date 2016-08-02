57e1229bbaf706a40c8638958ca45bb8ea2e27cc
Ronen Zilberman <ronen.zilberman@gmail.com>
2016-08-03 10:10:33 +1200

    put freq function within main file

--- EEGA_bands.m
+++ EEGA_bands.m
@@ -508,8 +508,18 @@ function segBand = calc_significance(realbandcorr, randbandcorrMulti)
 
 end
 
+%-----------------------------------------------------------------------------
+% Utility functions
 
+% convert to frequency domain
+function res = freq(x)
+	L=length(x);	 	 
+	NFFT=256;	 	 
+	X=fft(x,NFFT);	 	 
+	Px=X.*conj(X)/(NFFT*L); %Power of each freq components	 	 
+	res = Px(1:NFFT/2);
 
+end
 
 
 function result = cachefun(func, name)
--- freq.m
+++
@@ -1,8 +0,0 @@
-function res = freq(x)
-	L=length(x);	 	 
-	NFFT=256;	 	 
-	X=fft(x,NFFT);	 	 
-	Px=X.*conj(X)/(NFFT*L); %Power of each freq components	 	 
-	res = Px(1:NFFT/2);
-
-end
